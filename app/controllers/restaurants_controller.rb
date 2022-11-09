e3f3f4ec0fcce3c02bde478e9f9d8dc41643410b
Olivia Tangredi oliviatangredixo@gmail.com <oliviatangredixo@gmail.com>
2022-11-09 14:45:39 -0500

    new + create methods created and completed

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,6 +4,30 @@ class RestaurantsController < ApplicationController
   end
 
   def show
+    set_restaurant
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def set_restaurant
     @restaurant = Restaurant.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   # Read all
   get '/restaurants', to: 'restaurants#index'
 
+  # Create
+  # 1. Provide the form
+  get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # 2. Send the form
+  post '/restaurants', to: 'restaurants#create'
+
   # Read one
   get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
 end

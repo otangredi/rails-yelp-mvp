50f3e1c3f6112924fd5b25429570bba5375b4b75
Olivia Tangredi oliviatangredixo@gmail.com <oliviatangredixo@gmail.com>
2022-11-09 14:22:39 -0500

    index + show created and completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Read all
+  get '/restaurants', to: 'restaurants#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Read one
+  get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
 end

11d0df81a43c6829be5a27a6bd78e838e8f9021f
Olivia Tangredi oliviatangredixo@gmail.com <oliviatangredixo@gmail.com>
2022-11-09 16:46:10 -0500

    pushing before end of day

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   # Read all
-  get '/restaurants', to: 'restaurants#index'
+  # get '/restaurants', to: 'restaurants#index'
 
   # Create
-  # 1. Provide the form
-  get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
-  # 2. Send the form
-  post '/restaurants', to: 'restaurants#create'
+  # # 1. Provide the form
+  # get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # # 2. Send the form
+  # post '/restaurants', to: 'restaurants#create'
 
   # Read one
-  get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  resources :restaurants, only: %i[index show new create] do
+    resources :reviews, only: %i[new create]
+  end
 end
